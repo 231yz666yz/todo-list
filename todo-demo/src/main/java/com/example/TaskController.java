0d3a4fbef26a5c03da8dfc3cefc49b830070ad66
231yz666yz <x00217230143@njit.edu.cn>
2026-03-08 22:26:34 +0800

    数据初始化

--- todo-demo/src/main/java/com/example/TaskController.java
+++ todo-demo/src/main/java/com/example/TaskController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +21,19 @@ public class TaskController {
     @GetMapping
     public List<Task> getAllTasks(){
         return taskRepository.findAll();
+    }
+
+    @PostMapping
+    public Task creatTask(Task task){
+        return taskRepository.save(task);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, Task taskDetails){
+        Task task = taskRepository.findById(id).orElseThrow();
+        task.setTitle(taskDetails.getTitle());
+        task.setCompleted(taskDetails.isCompleted());
+        return taskRepository.save(task);
     }
 
     @DeleteMapping("/{id}")
